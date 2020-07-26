8274ebc84afc0bd2b3c1706775d55004e4028649
adminvm <vmartin@fednat.com>
2020-07-26 11:18:37 -0700

    Can now parse .fit files.

--- src/routes/rides/index.js
+++ src/routes/rides/index.js
@@ -1,24 +1,29 @@
 const express = require('express')
 const router = express.Router()
-const EasyFit = require('easy-fit/dist/easy-fit.js').default;
+const FitParser = require('fit-file-parser').default
 
 // Convert the fit file to some usable JSON.
+// Upload to DB.
 const processFile = ((file, callback) => {
-    const easyFit = new EasyFit({
+    const fitParser = new FitParser({
         force: true,
         speeddUnit: 'mph',
         lengthUnit: 'mi',
-        temperatureUnit: 'farhenheit'
+        temperatureUnit: 'farhenheit',
     })
 
-    return easyFit.parse(file, (error, data) => {
+    return fitParser.parse(file, (error, data) => {
         if(error) {
             throw(`Problem processing fit file ${file}`)
-        } 
+        }
+
+        // TODO: Upload to DB.
+
         callback(data)
     })
 })
 
+// TODO: Return all ride ids.
 router.get('/', (request, response) => {
     response.send({
         title: 'Ride 1',
@@ -26,6 +31,13 @@ router.get('/', (request, response) => {
         data: {miles: 10, feet: 200}
     })
 })
+
+// TODO: Upload ride to DB.
+// Allow the following:
+//  Device File: .FIT file for ride.
+//  Title: String
+//  Description: String
+//  Images: Jpeg. 
 
 router.post('/', (request, response) => {
     console.log('File upload request.') 
