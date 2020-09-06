4540565392d1115fc73fb326ffdfaa88675d4dbc
adminvm <vmartin@fednat.com>
2020-09-06 14:24:39 -0700

    Adding ability to save files via promises.

--- src/routes/rides/index.js
+++ src/routes/rides/index.js
@@ -3,23 +3,31 @@ const router = express.Router()
 const FitParser = require('fit-file-parser').default
 const Ride = require('../../models/ride')
 
-// Convert the fit file to some usable JSON.
-// Upload to DB.
-const processFitFile = ((file, callback) => {
-    const fitParser = new FitParser({
-        force: true,
-        speeddUnit: 'mph',
-        lengthUnit: 'mi',
-        temperatureUnit: 'farhenheit',
-    })
+const processFitPromise = (file) => {
+    console.log('***** processFitPromise running with file: ')
+    console.log(file)
 
-    return fitParser.parse(file, (error, data) => {
-        if(error) {
-            throw(`Problem processing fit file ${file}`)
-        }
-        callback(data)
+    return new Promise((resolve, reject) => {
+        const fitParser = new FitParser({
+            force: true,
+            speeddUnit: 'mph',
+            lengthUnit: 'mi',
+            temperatureUnit: 'farhenheit',
+        })
+
+        // TODO: Changed file to file.data
+        fitParser.parse(file.data, (error, data) => {
+            if(error) {
+                console.log('***** processFitPromise: Error hit') 
+                console.log(error)
+                reject(new Error('Problem processing fit file.'))
+            } else {
+                console.log('***** processFitPromise resolve trigger.')
+                resolve(data)
+            }
+        })
     })
-})
+}
 
 // Return a ride based on its document ID in the DB.
 router.get('/:id', (request, response) => {
@@ -42,31 +50,44 @@ router.get('/:id', (request, response) => {
 //  Images: Jpeg. 
 
 router.post('/', (request, response) => {
+    // Store ride data.
+    console.log('***** REQ *****')
+    console.log(request)
+    console.log('***** FILES *****')
+    console.log(request.files)
+    const ride = new Ride({
+        title: (request.title) ? request.title : 'Default Title.',
+        description: (request.description) ? request.description : 'This is a test ride.',
+        rideData: null,
+        image1: request.files.image1,
+        image2: request.files.image2,
+        image3: request.files.image3,
+        image4: request.files.image4,
+    })
+
     if (!request.files || Object.keys(request.files).length === 0) {
-        return response.status(400).send('No files were uploaded.');
+        return response.status(400).send('No files were attached.')
     } else {
-        processFitFile(request.files.rideFile.data, (data) => {
-            const ride = new Ride({
-                title: 'Test ride.',
-                description: 'This is a test ride.',
-                rideData: JSON.stringify(data),
-                image1: request.files.image1,
-                image2: request.files.image2,
-                image3: request.files.image3,
-                image4: request.files.image4,
+        if(request.files.rideFile) {
+            // We have a rideFile, go ahead and process it.
+            processFitPromise(request.files.rideFile).then(rideData => {
+                ride.rideData = JSON.stringify(rideData)
+            }).then(() => (ride.save())).then(doc => {
+                response.send(doc._id)
             })
-
-            ride.save((error, ride) => {
-                if(error) {
-                    console.error(error)
-                    response.status(400).send(error)
-                    return(error)
-                }
-
-                response.send(ride._id)})
-                console.log(ride)
+            .catch(error => {
+                console.log(error)
+                response.status(400).send(error)
             })
-    }
+        } else { // No rideFile was included.  Just save the non rideData stuff.
+            ride.save().then(doc => {
+                response.send(doc._id)
+           }).catch(error => {
+                console.log(error)
+                response.status(400).send(error)
+           })
+        }
+   }
 })
 
 module.exports = router 
